48407ba4128acb1ca7e55d41f02502c24045fa7b
Kacie Yates <kacieyates@mac.lan>
2026-03-17 14:27:04 -0400

    Remove supabaseAdmin from client supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,8 +4,3 @@ const supabaseUrl  = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnon)
-
-export const supabaseAdmin = createClient(
-  supabaseUrl,
-  process.env.SUPABASE_SERVICE_ROLE_KEY ?? ""
-)
