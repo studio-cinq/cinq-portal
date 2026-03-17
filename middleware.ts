7b99be84c4e1dad73a0ac9b64ebc0d51d048d416
Kacie Yates <kacieyates@mac.lan>
2026-03-17 13:18:34 -0400

    Fix implicit any on cookiesToSet in middleware

--- middleware.ts
+++ middleware.ts
@@ -13,7 +13,7 @@ export async function middleware(req: NextRequest) {
         getAll() {
           return req.cookies.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
           cookiesToSet.forEach(({ name, value, options }) => {
             req.cookies.set(name, value)
             res.cookies.set(name, value, options)
